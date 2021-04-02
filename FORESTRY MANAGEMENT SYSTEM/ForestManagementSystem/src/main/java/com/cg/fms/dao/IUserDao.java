1bc6b14cbc944d24fa9375ee7037914e58243aae
PrasanthMali <prasanthlions9@gmail.com>
2021-04-02 12:57:46 +0530

    First Commit

--- FORESTRY MANAGEMENT SYSTEM/ForestManagementSystem/src/main/java/com/cg/fms/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.cg.fms.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.fms.entity.User;
-
-public interface IUserDao extends JpaRepository<User, String> {
-	
-}
