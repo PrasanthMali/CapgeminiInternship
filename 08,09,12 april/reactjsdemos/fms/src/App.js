08a748b7fc9e2ea0564a61b0595f5c49d32c99c1
PrasanthMali <prasanthlions9@gmail.com>
2021-04-12 17:42:52 +0530

    First Commit

--- 08,09,12 april/reactjsdemos/fms/src/App.js
+++ 08,09,12 april/reactjsdemos/fms/src/App.js
@@ -1,13 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
-import FormControl4 from './components/formcontrol4';
-import UpdateAdmin from './components/updateReq';
-import Routes from './Route';
-
+// import FormControl4 from './components/formcontrol4';
+// import UpdateAdmin from './components/updateReq';
+// import Routes from './Route';
+import LikesCount from './components/hoc/LikesCount';
+import CommentsCount from './components/hoc/CommentCount';
+import NameFormComponent from './components/ref/NameFormComponent';
 
 function App() {
-  return (    
-    <Routes/>       
+  return (  
+    <div>
+      <CommentsCount/>
+      <LikesCount/>
+      <NameFormComponent/>     
+    </div>   
+    // <Routes/>       
     // <FormControl4/>
     // <UpdateAdmin/>
     // <AdminComponent/> 
